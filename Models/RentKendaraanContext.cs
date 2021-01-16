2b19efd908d706384af9825f53625886fffa8a29
Akbarsuper889 <sempakgaring889@gmail.com>
2021-01-16 20:26:34 +0700

    Add project files.

---
+++ Models/RentKendaraanContext.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace RentalKendaraan_20180140013.Models
+{
+    public partial class RentKendaraanContext : IdentityDbContext
+    {
+        public RentKendaraanContext()
+        {
+        }
+
+        public RentKendaraanContext(DbContextOptions<RentKendaraanContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<AspNetRoleClaims> AspNetRoleClaims { get; set; }
+        public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaims> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogins> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUserRoles> AspNetUserRoles { get; set; }
+        public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
+        public virtual DbSet<AspNetUserTokens> AspNetUserTokens { get; set; }
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<Gender> Gender { get; set; }
+        public virtual DbSet<Jaminan> Jaminan { get; set; }
+        public virtual DbSet<JenisKendaraan> JenisKendaraan { get; set; }
+        public virtual DbSet<Kendaraan> Kendaraan { get; set; }
+        public virtual DbSet<KondisiKendaraan> KondisiKendaraan { get; set; }
+        public virtual DbSet<Peminjaman> Peminjaman { get; set; }
+        public virtual DbSet<Pengembalian> Pengembalian { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AspNetRoleClaims>(entity =>
+            {
+                entity.HasIndex(e => e.RoleId);
+
+                entity.Property(e => e.RoleId).IsRequired();
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.AspNetRoleClaims)
+                    .HasForeignKey(d => d.RoleId);
+            });
+
+            modelBuilder.Entity<AspNetRoles>(entity =>
+            {
+                entity.HasIndex(e => e.NormalizedName)
+                    .HasName("RoleNameIndex")
+                    .IsUnique()
+                    .HasFilter("([NormalizedName] IS NOT NULL)");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name).HasMaxLength(256);
+
+                entity.Property(e => e.NormalizedName).HasMaxLength(256);
+            });
+
+            modelBuilder.Entity<AspNetUserClaims>(entity =>
+            {
+                entity.HasIndex(e => e.UserId);
+
+                entity.Property(e => e.UserId).IsRequired();
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AspNetUserClaims)
+                    .HasForeignKey(d => d.UserId);
+            });
+
+            modelBuilder.Entity<AspNetUserLogins>(entity =>
+            {
+                entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
+
+                entity.HasIndex(e => e.UserId);
+
+                entity.Property(e => e.LoginProvider).HasMaxLength(128);
+
+                entity.Property(e => e.ProviderKey).HasMaxLength(128);
+
+                entity.Property(e => e.UserId).IsRequired();
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AspNetUserLogins)
+                    .HasForeignKey(d => d.UserId);
+            });
+
+            modelBuilder.Entity<AspNetUserRoles>(entity =>
+            {
+                entity.HasKey(e => new { e.UserId, e.RoleId });
+
+                entity.HasIndex(e => e.RoleId);
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.AspNetUserRoles)
+                    .HasForeignKey(d => d.RoleId);
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AspNetUserRoles)
+                    .HasForeignKey(d => d.UserId);
+            });
+
+            modelBuilder.Entity<AspNetUsers>(entity =>
+            {
+                entity.HasIndex(e => e.NormalizedEmail)
+                    .HasName("EmailIndex");
+
+                entity.HasIndex(e => e.NormalizedUserName)
+                    .HasName("UserNameIndex")
+                    .IsUnique()
+                    .HasFilter("([NormalizedUserName] IS NOT NULL)");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Email).HasMaxLength(256);
+
+                entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
+
+                entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
+
+                entity.Property(e => e.UserName).HasMaxLength(256);
+            });
+
+            modelBuilder.Entity<AspNetUserTokens>(entity =>
+            {
+                entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name });
+
+                entity.Property(e => e.LoginProvider).HasMaxLength(128);
+
+                entity.Property(e => e.Name).HasMaxLength(128);
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AspNetUserTokens)
+                    .HasForeignKey(d => d.UserId);
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(e => e.IdCustomer);
+
+                entity.Property(e => e.IdCustomer)
+                    .HasColumnName("ID_Customer")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Alamat)
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IdGender).HasColumnName("ID_Gender");
+
+                entity.Property(e => e.NamaCustomer)
+                    .HasColumnName("Nama_Customer")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nik)
+                    .HasColumnName("NIK")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NoHp)
+                    .HasColumnName("No_HP")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdGenderNavigation)
+                    .WithMany(p => p.Customer)
+                    .HasForeignKey(d => d.IdGender)
+                    .HasConstraintName("FK_Customer_Gender");
+            });
+
+            modelBuilder.Entity<Gender>(entity =>
+            {
+                entity.HasKey(e => e.IdGender);
+
+                entity.Property(e => e.IdGender)
+                    .HasColumnName("ID_Gender")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.NamaGender)
+                    .HasColumnName("Nama_Gender")
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Jaminan>(entity =>
+            {
+                entity.HasKey(e => e.IdJaminan);
+
+                entity.Property(e => e.IdJaminan)
+                    .HasColumnName("ID_Jaminan")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.NamaJaminan)
+                    .HasColumnName("Nama_Jaminan")
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<JenisKendaraan>(entity =>
+            {
+                entity.HasKey(e => e.IdJenisKendaraan);
+
+                entity.ToTable("Jenis_Kendaraan");
+
+                entity.Property(e => e.IdJenisKendaraan)
+                    .HasColumnName("ID_Jenis_Kendaraan")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.NamaJenisKendaraan)
+                    .HasColumnName("Nama_Jenis_Kendaraan")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Kendaraan>(entity =>
+            {
+                entity.HasKey(e => e.IdKendaraan);
+
+                entity.Property(e => e.IdKendaraan)
+                    .HasColumnName("ID_Kendaraan")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.IdJenisKendaraan).HasColumnName("ID_Jenis_Kendaraan");
+
+                entity.Property(e => e.Ketersediaan)
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NamaKendaraan)
+                    .HasColumnName("Nama_Kendaraan")
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NoPolisi)
+                    .HasColumnName("No_Polisi")
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NoStnk)
+                    .HasColumnName("No_STNK")
+                    .HasMaxLength(8)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdJenisKendaraanNavigation)
+                    .WithMany(p => p.Kendaraan)
+                    .HasForeignKey(d => d.IdJenisKendaraan)
+                    .HasConstraintName("FK_Kendaraan_Jenis_Kendaraan");
+            });
+
+            modelBuilder.Entity<KondisiKendaraan>(entity =>
+            {
+                entity.HasKey(e => e.IdKondisi);
+
+                entity.ToTable("Kondisi_Kendaraan");
+
+                entity.Property(e => e.IdKondisi)
+                    .HasColumnName("ID_Kondisi")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.NamaKondisi)
+                    .HasColumnName("Nama_Kondisi")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Peminjaman>(entity =>
+            {
+                entity.HasKey(e => e.IdPeminjaman);
+
+                entity.Property(e => e.IdPeminjaman)
+                    .HasColumnName("ID_Peminjaman")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.IdCustomer).HasColumnName("ID_Customer");
+
+                entity.Property(e => e.IdJaminan).HasColumnName("ID_Jaminan");
+
+                entity.Property(e => e.IdKendaraan).HasColumnName("ID_Kendaraan");
+
+                entity.Property(e => e.TglPeminjaman)
+                    .HasColumnName("Tgl_Peminjaman")
+                    .HasColumnType("datetime");
+
+                entity.HasOne(d => d.IdCustomerNavigation)
+                    .WithMany(p => p.Peminjaman)
+                    .HasForeignKey(d => d.IdCustomer)
+                    .HasConstraintName("FK_Peminjaman_Customer");
+
+                entity.HasOne(d => d.IdJaminanNavigation)
+                    .WithMany(p => p.Peminjaman)
+                    .HasForeignKey(d => d.IdJaminan)
+                    .HasConstraintName("FK_Peminjaman_Jaminan");
+
+                entity.HasOne(d => d.IdKendaraanNavigation)
+                    .WithMany(p => p.Peminjaman)
+                    .HasForeignKey(d => d.IdKendaraan)
+                    .HasConstraintName("FK_Peminjaman_Kendaraan");
+            });
+
+            modelBuilder.Entity<Pengembalian>(entity =>
+            {
+                entity.HasKey(e => e.IdPengembalian);
+
+                entity.Property(e => e.IdPengembalian)
+                    .HasColumnName("ID_Pengembalian")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.IdKondisi).HasColumnName("ID_Kondisi");
+
+                entity.Property(e => e.IdPeminjaman).HasColumnName("ID_Peminjaman");
+
+                entity.Property(e => e.TglPengembalian)
+                    .HasColumnName("Tgl_Pengembalian")
+                    .HasColumnType("datetime");
+
+                entity.HasOne(d => d.IdKondisiNavigation)
+                    .WithMany(p => p.Pengembalian)
+                    .HasForeignKey(d => d.IdKondisi)
+                    .HasConstraintName("FK_Pengembalian_Kondisi_Kendaraan");
+
+                entity.HasOne(d => d.IdPeminjamanNavigation)
+                    .WithMany(p => p.Pengembalian)
+                    .HasForeignKey(d => d.IdPeminjaman)
+                    .HasConstraintName("FK_Pengembalian_Peminjaman");
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        internal static Task<string> ToListAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
